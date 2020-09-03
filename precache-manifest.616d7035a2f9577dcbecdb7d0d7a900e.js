b575cd61184158eff7e345d787edab2309b2ea0a
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-09-02 19:58:08 -0700

    Updates

---
+++ precache-manifest.616d7035a2f9577dcbecdb7d0d7a900e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ab108a8f98f2d634171ba44ad2094a",
+    "url": "/game-of-life-build/index.html"
+  },
+  {
+    "revision": "1ce7614b3b90a9bf2554",
+    "url": "/game-of-life-build/static/css/main.198cf5ae.chunk.css"
+  },
+  {
+    "revision": "84004774751c2037cedd",
+    "url": "/game-of-life-build/static/js/2.202f34f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life-build/static/js/2.202f34f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce7614b3b90a9bf2554",
+    "url": "/game-of-life-build/static/js/main.ab5fa647.chunk.js"
+  },
+  {
+    "revision": "d1ad9e66b6a702515a52",
+    "url": "/game-of-life-build/static/js/runtime-main.61d88cb0.js"
+  },
+  {
+    "revision": "2ec3be004b6175f89dfce7b17fe219e2",
+    "url": "/game-of-life-build/static/media/img_background.2ec3be00.png"
+  }
+]);
